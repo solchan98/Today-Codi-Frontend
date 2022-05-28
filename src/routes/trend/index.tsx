5e549722c5d2f0352a7db2545b32efe18b28f047
solchan98 <dev.sol0427@gmail.com>
2022-05-28 20:06:22 +0900

    feat: 트렌드 페이지 드랍다운 및 카드 레이아웃 구성 작업(세부 디테일링 필요)

--- src/routes/trend/index.tsx
+++ src/routes/trend/index.tsx
@@ -1,13 +1,30 @@
 import DropDown from '../../components/common/DropDown';
 import Card from '../../components/trend/Card';
 
+import cs from './trend.module.scss';
+
 const TEMP_SEX_WORD_LIST = ['전체', '남', '여'];
+const TEMP_AGE_WORD_LIST = ['10대', '20대', '30대', '40대', '50대'];
+const TEMP_STYLE_WORD_LIST = ['무난', '화려', '정장', '대학생', '파티'];
 
 const Trend = () => {
   return (
-    <div>
-      {/* <DropDown title='성별' valList={TEMP_SEX_WORD_LIST} /> */}
-      <Card />
+    <div className={cs.trend}>
+      <div className={cs.dropDownWrapper}>
+        <DropDown title='성별' valList={TEMP_SEX_WORD_LIST} />
+        <DropDown title='나이' valList={TEMP_AGE_WORD_LIST} />
+        <DropDown title='스타일' valList={TEMP_STYLE_WORD_LIST} />
+      </div>
+      <div className={cs.cardWrapper}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
