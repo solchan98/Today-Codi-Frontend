b4ad9a5cf4604c234cbc0361009506d3e20ffa6e
solchan98 <dev.sol0427@gmail.com>
2022-05-29 19:04:08 +0900

    feat: 트렌드 상세 페이지 기본 구조 작업 및 해시태그 컴포넌트 구현

---
+++ src/components/common/HashTag/index.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import cs from './hashTag.module.scss';
+
+interface Props {
+  name: string;
+}
+
+const HashTag = ({ name }: Props) => {
+  const nav = useNavigate();
+
+  const onHashTagClick = () => {
+    // TODO: 해시태그 값을 통해 검색 결과 페이지로 이동시킨다.
+    nav('/');
+  };
+  return (
+    <button className={cs.hashTag} type='button' onClick={onHashTagClick}>
+      #{name}
+    </button>
+  );
+};
+
+export default HashTag;
--- src/routes/board/index.tsx
+++ src/routes/board/index.tsx
@@ -1,9 +1,34 @@
 import MainPoster from '../../components/board/MainPoster/inedx';
+import cs from './board.module.scss';
+import HashTag from '../../components/common/HashTag';
 
 const Board = () => {
   return (
-    <div>
-      <MainPoster />
+    <div className={cs.boardWrapper}>
+      <div>
+        <div className={cs.boardHeader}>
+          <ul>
+            {/* TODO: TAG MAX COUNT 16 */}
+            <HashTag name='코디' />
+            <HashTag name='대학생' />
+            <HashTag name='트렌드' />
+            <HashTag name='코디' />
+            <HashTag name='대학생' />
+            <HashTag name='트렌드' />
+            <HashTag name='코디' />
+            <HashTag name='대학생' />
+            <HashTag name='트렌드' />
+            <HashTag name='코디' />
+            <HashTag name='대학생' />
+            <HashTag name='트렌드' />
+            <HashTag name='코디' />
+            <HashTag name='대학생' />
+          </ul>
+          <time>2022-05-12</time>
+        </div>
+        <MainPoster />
+      </div>
+      <aside className={cs.asideWrapper} />
     </div>
   );
 };
