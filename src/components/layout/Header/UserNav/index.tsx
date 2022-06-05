5dce857737bdda7ef001c9f153a14bd1d7d7b9c2
solchan98 <dev.sol0427@gmail.com>
2022-06-05 12:45:30 +0900

    chore: 프로필 클릭 시, 로그아웃 구현 롤백( 재로그인 시 axios token값에 , 붙는 버그 발견)

--- src/components/layout/Header/UserNav/index.tsx
+++ src/components/layout/Header/UserNav/index.tsx
@@ -1,20 +1,14 @@
 import { useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from 'redux/store';
+import { useAppSelector } from 'redux/store';
 
 import cs from './userNav.module.scss';
 import { DEFAULT_IMG_URL } from 'constant/base';
-import { initUser } from '../../../../redux/store/slices/userSlice';
 
 const UserNav = () => {
   const { profileImg } = useAppSelector((state) => state.user);
-  const dispatch = useAppDispatch();
   const nav = useNavigate();
   const onClick = () => {
     nav('/new-post');
-  };
-
-  const onClickProfile = () => {
-    dispatch(initUser());
   };
 
   return (
@@ -24,7 +18,6 @@ const UserNav = () => {
         style={{ width: '36px', height: '36px', backgroundImage: `url(${profileImg ?? DEFAULT_IMG_URL})` }}
         type='button'
         aria-label='profile_btn'
-        onClick={onClickProfile}
       />
       <button type='button' onClick={onClick}>
         글쓰기
